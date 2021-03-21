32d36bd26da2e20eea20367ae96bd80489323aad
arthurdoelp <arthurdoelp@gmail.com>
2021-03-21 17:36:33 -0400

    moved the buildpack up on the list. Maybe that will improve the success

--- controllers/scrape.controller.js
+++ controllers/scrape.controller.js
@@ -9,10 +9,10 @@ exports.newScrapeController = (req, res) => {
     const pythonProcess = spawn('python', ["python/scrape.py"]);
 
     pythonProcess.stdout.on('data', (data) => {
-        var buf = JSON.stringify(data.toString());
-        console.log(buf)
-        var formatted_data = JSON.parse(buf);
-        console.log(formatted_data)
+        // var buf = JSON.stringify(data.toString());
+        // console.log(buf)
+        // var formatted_data = JSON.parse(buf);
+        // console.log(formatted_data)
 
         res.json({ data: "Success!" })
     })
